c4278fdba8775d14ef9cfd07bc804329fdfb1f6f
eurdream98 <eurdream98@naver.com>
2024-03-17 00:03:27 +0900

    2024_03_17 Controller 식별자로 정보 조회,수정,삭제 테스트기능 추가

--- src/test/java/com/ohgiraffers/crud/hospital/controller/PatientControllerTests.java
+++ src/test/java/com/ohgiraffers/crud/hospital/controller/PatientControllerTests.java
@@ -2,15 +2,18 @@ package com.ohgiraffers.crud.hospital.controller;
 
 import com.ohgiraffers.crud.configuration.application.CrudHomeworkApplication;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.client.RequestMatcher;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
@@ -44,6 +47,42 @@ public class PatientControllerTests {
         params.add("patientAdmission","Y");
         params.add("departmentCode","3");
         mockMvc.perform(MockMvcRequestBuilders.post("/patient/insert").params(params))
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/patient/selectAll"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("식별자로 환자 정보 조회해오는 테스트")
+    public void selectOne() throws Exception {
+        MultiValueMap<String,String> param = new LinkedMultiValueMap<>();
+        param.add("patientCode","3");
+        mockMvc.perform(MockMvcRequestBuilders.post("/patient/selectOne").params(param))
+                .andExpect(MockMvcResultMatchers.forwardedUrl("/result/one"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("환자의 정보를 수정하는 테스트")
+    public void updatePatient() throws Exception {
+        MultiValueMap<String,String> param = new LinkedMultiValueMap<>();
+        param.add("patientAge","5");
+        param.add("patientName","고동환");
+        mockMvc.perform(MockMvcRequestBuilders.post("/patient/updateOne").params(param))
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/patient/selectAll"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("식별자로 환자 정보 삭제")
+    public void deleteOne() throws Exception {
+        MultiValueMap<String,String> param = new LinkedMultiValueMap<>();
+        param.add("patientCode","4");
+        mockMvc.perform(MockMvcRequestBuilders.post("/patient/deleteOne").params(param))
                 .andExpect(MockMvcResultMatchers.redirectedUrl("/patient/selectAll"))
                 .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
                 .andDo(MockMvcResultHandlers.print());
